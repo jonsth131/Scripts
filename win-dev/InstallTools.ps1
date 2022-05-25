fcd4c4a6de227b66136d60c61330cacd15992c05
Jonas S <jonsth131@users.noreply.github.com>
2022-05-25 13:17:56 +0200

    Create InstallTools.ps1

---
+++ win-dev/InstallTools.ps1
@@ -0,0 +1,72 @@
+$GeneralTools = @(
+    'cmder',
+    'googlechrome',
+    'firefox',
+    'microsoft-windows-terminal',
+    '7zip',
+    'notepadplusplus'
+)
+
+$DevTools = @(
+    'git',
+    'jetbrains-toolbox',
+    'jetbrains-rider',
+    'vscode',
+    'postman',
+    'dotnet-sdk',
+    'azure-cli',
+    'azure-functions-core-tools',
+    'microsoftazurestorageexplorer',
+    'azure-cosmosdb-emulator',
+    'docker-desktop'
+)
+
+$SecurityTools = @(
+    'zap',
+    'burp-suite-free-edition'
+)
+
+function Invoke-Choco {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string] $PackageName
+    )
+
+    & choco install $PackageName
+}
+
+function Install-Packages {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string[]] $Packages
+    )
+
+    foreach ($item in $Packages) {
+        Invoke-Choco $item
+    }
+}
+
+Write-Host "Installing Debian in WSL"
+& wsl --install -d Debian
+
+$testchoco = powershell choco -v
+if(-not($testchoco)){
+    Write-Host "Seems Chocolatey is not installed, installing now"
+    Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+else{
+    Write-Host "Chocolatey Version $testchoco is already installed"
+}
+
+& choco feature enable -n allowGlobalConfirmation
+
+Write-Host "Installing General Tools"
+Install-Packages $GeneralTools
+
+Write-Host "Installing Dev Tools"
+Install-Packages $DevTools
+
+Write-Host "Installing Security Tools"
+Install-Packages $SecurityTools
